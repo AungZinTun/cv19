5135045105bd123476f9b92eb2ed7f75c484dac8
unknown <augzintun2@gmail.com>
2020-04-23 00:14:46 +0630

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div class="hello">
+    <h1 v-for="thread in threads">
+       {{ thread.title }}
+      <p v-for="postID in thread.posts"> 
+        {{ posts[postID].userID}}
+         {{posts[postID].text}}
+        </p>   
+    </h1>
+    
+  </div>
+
+</template>
+
+<script>
+import datasource from '@/data.json'
+
+export default {
+  name: 'home',
+  data () {
+    return {
+      threads: datasource.threads,
+      posts: datasource.posts,
+      users : datasource.users
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
